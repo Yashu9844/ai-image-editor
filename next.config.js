dfa2468b770ac3694a3bb68a422b8b379b3d2bb3
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2025-10-14 00:27:05 +0530

    build errror

--- next.config.js
+++ next.config.js
@@ -7,9 +7,7 @@ import "./src/env.js";
 /** @type {import("next").NextConfig} */
 const config = {
   experimental: {
-    allowedDevOrigins: [
-      '2b0a32628d1c3b86f0de77e994253e60.serveo.net',
-    ],
+    
   },
 };
 
